2f82ca94b0bc5d5c71754b757b838e443cacca60
henning mueller <henning@orgizm.net>
2014-10-07 14:39:01 +0200

    Coveralls, you back?

--- lib/tasks/test.rb
+++ lib/tasks/test.rb
@@ -2,10 +2,9 @@ namespace :test do
   desc 'Run all test suites and push coverage data to Coveralls'
   task :coveralls do
     require 'coveralls'
+    Coveralls.wear!
 
     Rake::Task['spec'].invoke
     Rake::Task['test'].invoke
-
-    Coveralls.push!
   end
 end
